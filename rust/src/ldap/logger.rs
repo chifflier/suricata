e3ce98efecbccc394399af7b1ed294abc0986188
Pierre Chifflier <pierre.chifflier@ssi.gouv.fr>
2022-12-06 14:27:05 +0100

    rust/LDAP: add configuration for logger, allow selecting LDAP operations

--- rust/src/ldap/logger.rs
+++ rust/src/ldap/logger.rs
@@ -18,8 +18,74 @@
 // written by Pierre Chifflier  <chifflier@wzdftpd.net>
 
 use super::ldap::{LDAPState, LDAPTransaction};
+use crate::conf::ConfNode;
 use crate::jsonbuilder::{JsonBuilder, JsonError};
+use ldap_parser::ldap::ProtocolOpTag;
+use std::os::raw::c_void;
 use std::string::String;
+
+pub struct LDAPLogger {
+    /// Bitfield representing one operation per bit.
+    /// There are only 26 operations in LDAP RFC
+    operations: u32,
+}
+
+impl LDAPLogger {
+    pub fn new(conf: ConfNode) -> Self {
+        // default value: everything is logged
+        let mut operations = 0;
+        if let Some(op_list) = conf.get_child_value("operations") {
+            for op_str in op_list.split(',') {
+                match ldap_str_to_operation(op_str.trim()) {
+                    Some(op) => {
+                        operations |= 1 << op.0;
+                    }
+                    None => {
+                        SCLogError!("LDAP/Logger: invalid operation {}", op_str);
+                    }
+                }
+            }
+        } else {
+            operations = 0xffff_ffff;
+        }
+        Self { operations }
+    }
+
+    pub fn do_log(&self, tx: &LDAPTransaction) -> bool {
+        if self.operations & (1 << tx.request_protocol_op.0) != 0 {
+            return true;
+        }
+        // other conditions?
+        return false;
+    }
+}
+
+fn ldap_str_to_operation(s: &str) -> Option<ProtocolOpTag> {
+    match s {
+        "BindRequest" => Some(ProtocolOpTag::BindRequest),
+        "BindResponse" => Some(ProtocolOpTag::BindResponse),
+        "UnbindRequest" => Some(ProtocolOpTag::UnbindRequest),
+        "SearchRequest" => Some(ProtocolOpTag::SearchRequest),
+        "SearchResultEntry" => Some(ProtocolOpTag::SearchResultEntry),
+        "SearchResultDone" => Some(ProtocolOpTag::SearchResultDone),
+        "ModifyRequest" => Some(ProtocolOpTag::ModifyRequest),
+        "ModifyResponse" => Some(ProtocolOpTag::ModifyResponse),
+        "AddRequest" => Some(ProtocolOpTag::AddRequest),
+        "AddResponse" => Some(ProtocolOpTag::AddResponse),
+        "DelRequest" => Some(ProtocolOpTag::DelRequest),
+        "DelResponse" => Some(ProtocolOpTag::DelResponse),
+        "ModDnRequest" => Some(ProtocolOpTag::ModDnRequest),
+        "ModDnResponse" => Some(ProtocolOpTag::ModDnResponse),
+        "CompareRequest" => Some(ProtocolOpTag::CompareRequest),
+        "CompareResponse" => Some(ProtocolOpTag::CompareResponse),
+        "AbandonRequest" => Some(ProtocolOpTag::AbandonRequest),
+        "SearchResultReference" => Some(ProtocolOpTag::SearchResultReference),
+        "ExtendedRequest" => Some(ProtocolOpTag::ExtendedRequest),
+        "ExtendedResponse" => Some(ProtocolOpTag::ExtendedResponse),
+        "IntermediateResponse" => Some(ProtocolOpTag::IntermediateResponse),
+        _ => None,
+    }
+}
 
 fn ldap_log_response(jsb: &mut JsonBuilder, tx: &LDAPTransaction) -> Result<(), JsonError> {
     jsb.set_string("request_protocol_op", &tx.request_protocol_op.to_string())?;
@@ -28,7 +94,7 @@ fn ldap_log_response(jsb: &mut JsonBuilder, tx: &LDAPTransaction) -> Result<(), 
             // special display for empty bind DN
             jsb.set_string("bind_dn", "<ROOT>")?;
         } else {
-            jsb.set_string("bind_dn", &dn)?;
+            jsb.set_string("bind_dn", dn)?;
         }
     }
     if let Some(pwd) = &tx.bind_pwd {
@@ -36,7 +102,7 @@ fn ldap_log_response(jsb: &mut JsonBuilder, tx: &LDAPTransaction) -> Result<(), 
         jsb.set_string("bind_pwd", &s)?;
     }
     if let Some(s) = &tx.sasl_mech {
-        jsb.set_string("sasl_mech", &s)?;
+        jsb.set_string("sasl_mech", s)?;
     }
     if let Some(id) = &tx.message_id {
         jsb.set_string("message_id", &id.0.to_string())?;
@@ -52,4 +118,24 @@ pub extern "C" fn rs_ldap_log_json_response(
     jsb: &mut JsonBuilder, _state: &mut LDAPState, tx: &mut LDAPTransaction,
 ) -> bool {
     ldap_log_response(jsb, tx).is_ok()
+}
+
+#[no_mangle]
+pub extern "C" fn rs_ldap_logger_new(conf: *const c_void) -> *mut std::os::raw::c_void {
+    let conf = ConfNode::wrap(conf);
+    let boxed = Box::new(LDAPLogger::new(conf));
+    return Box::into_raw(boxed) as *mut _;
+}
+#[no_mangle]
+pub unsafe extern "C" fn rs_ldap_logger_free(logger: *mut std::os::raw::c_void) {
+    std::mem::drop(Box::from_raw(logger as *mut LDAPLogger));
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn rs_ldap_logger_do_log(
+    logger: *mut std::os::raw::c_void, tx: *mut std::os::raw::c_void,
+) -> bool {
+    let logger = cast_pointer!(logger, LDAPLogger);
+    let tx = cast_pointer!(tx, LDAPTransaction);
+    logger.do_log(tx)
 }
--- src/output-json-ldap.c
+++ src/output-json-ldap.c
@@ -48,13 +48,28 @@
 
 #include "rust.h"
 
-static int JsonLdapLogger(ThreadVars *tv, void *thread_data,
+typedef struct LogLDAPCtx_ {
+    void *rs_logger;
+    OutputJsonCtx *eve_ctx;
+} LogLDAPCtx;
+
+typedef struct LogLDAPLogThread_ {
+    LogLDAPCtx *ldap_ctx;
+    OutputJsonThreadCtx *thread;
+} LogLDAPLogThread;
+
+static int JsonLDAPLogger(ThreadVars *tv, void *thread_data,
     const Packet *p, Flow *f, void *state, void *tx, uint64_t tx_id)
 {
     LDAPTransaction *ldaptx = tx;
-    OutputJsonThreadCtx *thread = thread_data;
+    LogLDAPLogThread *thread = thread_data;
+    LogLDAPCtx *ctx = thread->ldap_ctx;
 
-    JsonBuilder *jb = CreateEveHeader(p, LOG_DIR_PACKET, "ldap", NULL, thread->ctx);
+    if (!rs_ldap_logger_do_log(ctx->rs_logger, tx)) {
+        return TM_ECODE_OK;
+    }
+
+    JsonBuilder *jb = CreateEveHeader(p, LOG_DIR_PACKET, "ldap", NULL, ctx->eve_ctx);
     if (unlikely(jb == NULL)) {
         return TM_ECODE_FAILED;
     }
@@ -65,7 +80,7 @@ static int JsonLdapLogger(ThreadVars *tv, void *thread_data,
     }
     jb_close(jb);
 
-    OutputJsonBuilderBuffer(jb, thread);
+    OutputJsonBuilderBuffer(jb, thread->thread);
 
     jb_free(jb);
     return TM_ECODE_OK;
@@ -75,20 +90,79 @@ error:
     return TM_ECODE_FAILED;
 }
 
+
+static void OutputLDAPLogDeInitCtxSub(OutputCtx *output_ctx)
+{
+    LogLDAPCtx *ldap_ctx = (LogLDAPCtx *)output_ctx->data;
+    rs_dhcp_logger_free(ldap_ctx->rs_logger);
+    SCFree(ldap_ctx);
+    SCFree(output_ctx);
+}
+
 static OutputInitResult OutputLdapLogInitSub(ConfNode *conf,
     OutputCtx *parent_ctx)
 {
+    OutputInitResult result = { NULL, false };
+
+    LogLDAPCtx *ldap_ctx = SCCalloc(1, sizeof(*ldap_ctx));
+    if (unlikely(ldap_ctx == NULL)) {
+        return result;
+    }
+    ldap_ctx->eve_ctx = parent_ctx->data;
+
+    OutputCtx *output_ctx = SCCalloc(1, sizeof(*output_ctx));
+    if (unlikely(output_ctx == NULL)) {
+        SCFree(ldap_ctx);
+        return result;
+    }
+    output_ctx->data = ldap_ctx;
+    output_ctx->DeInit = OutputLDAPLogDeInitCtxSub;
+
+    ldap_ctx->rs_logger = rs_ldap_logger_new(conf);
+
     AppLayerParserRegisterLogger(IPPROTO_TCP, ALPROTO_LDAP);
     AppLayerParserRegisterLogger(IPPROTO_UDP, ALPROTO_LDAP);
-    return OutputJsonLogInitSub(conf, parent_ctx);
+
+    result.ctx = output_ctx;
+    result.ok = true;
+    return result;
+}
+
+static TmEcode JsonLDAPLogThreadInit(ThreadVars *t, const void *initdata, void **data)
+{
+    LogLDAPLogThread *thread = SCCalloc(1, sizeof(*thread));
+    if (unlikely(thread == NULL)) {
+        return TM_ECODE_FAILED;
+    }
+    LogLDAPCtx *ctx = ((OutputCtx *)initdata)->data;
+    thread->ldap_ctx = ctx;
+    thread->thread = CreateEveThreadCtx(t, ctx->eve_ctx);
+    if (thread->thread == NULL) {
+        SCFree(thread);
+        return TM_ECODE_FAILED;
+    }
+
+    *data = (void *)thread;
+    return TM_ECODE_OK;
+}
+
+static TmEcode JsonLDAPLogThreadDeinit(ThreadVars *t, void *data)
+{
+    LogLDAPLogThread *thread = (LogLDAPLogThread *)data;
+    if (thread == NULL) {
+        return TM_ECODE_OK;
+    }
+    FreeEveThreadCtx(thread->thread);
+    SCFree(thread);
+    return TM_ECODE_OK;
 }
 
 void JsonLdapLogRegister(void)
 {
     /* Register as an eve sub-module. */
     OutputRegisterTxSubModule(LOGGER_JSON_TX, "eve-log", "JsonLdapLog", "eve-log.ldap",
-        OutputLdapLogInitSub, ALPROTO_LDAP, JsonLdapLogger, JsonLogThreadInit,
-        JsonLogThreadDeinit, NULL);
+        OutputLdapLogInitSub, ALPROTO_LDAP, JsonLDAPLogger, JsonLDAPLogThreadInit,
+        JsonLDAPLogThreadDeinit, NULL);
 
     SCLogNotice("Ldap JSON logger registered.");
 }
